4099eee6569358c83ccb5106f1c8a961162ff29b
Lane Humphreys <lane.humphreys12@gmail.com>
2024-09-19 15:15:16 -0400

    cool

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,14 +1,15 @@
-import "./Projects.css"
+import "./Projects.css";
 import { cueImage } from "../../assets";
 
 const Projects = () => {
-    return (
-        <>
-        <div className="projects-container">
-            
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="projects-container">
+        <div className="project-item">Hello</div>
+        <div className="project-item">Hello</div>
+      </div>
+    </>
+  );
+};
 
 export default Projects;
--- src/components/aboutme/AboutMe.jsx
+++ src/components/aboutme/AboutMe.jsx
@@ -6,23 +6,27 @@ const AboutMe = () => {
       <div className="aboutMe">
         <div className="aboutMe-content">
           <h1>About Me</h1>
-          <div>Hello, I am a comp TIA certified Cyber Security Specialist, that also happens to be an avid programmer. I have been interested in technology since I was young, and
-            my passion has kept growing ever since.
-          </div>
-          <br/>
           <div>
-            My preferred languages:
+            Hello, I am a comp TIA certified Cyber Security Specialist, that
+            also happens to be an avid programmer. I have been interested in
+            technology since I was young, and my passion has kept growing ever
+            since.
+          </div>
+          <br />
+          <div>
+            My preferred technologies and languages:
             <ul>
-            <li>Lua</li>
-            <li>Python</li>
-            <li>TypeScript</li>
-            <li>React</li>
-            <li>Vite</li>
-          </ul>
+              <li>Lua</li>
+              <li>Python</li>
+              <li>TypeScript</li>
+              <li>React</li>
+              <li>Vite</li>
+            </ul>
           </div>
-          <br/>
+          <br />
           <div>
-            While these are my preferred languages and technologies, I am still open to learning and using whatever else to get the job done!
+            While these are my preferred languages and technologies, I am still
+            open to learning and using whatever else to get the job done!
           </div>
         </div>
       </div>
